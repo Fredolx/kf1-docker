23407fa316e154ed5fba60907347c0aad51ec002
Frederic Lachapelle <frederic.lachapelle7@gmail.com>
2026-05-21 15:26:49 -0400

    turn off VAC

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /server
-exec wine system/ucc.exe server KF-bioticslab.rom?game=KFmod.KFGameType?VACSecured=true?MaxPlayers=6?AdminName=admin?AdminPassword=$ADMIN_PASSWORD
+exec wine system/ucc.exe server KF-bioticslab.rom?game=KFmod.KFGameType?VACSecured=false?MaxPlayers=6?AdminName=admin?AdminPassword=$ADMIN_PASSWORD
