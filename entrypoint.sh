e12b2850e07871642e4eababfb08b1d0257fc294
Frederic Lachapelle <frederic.lachapelle7@gmail.com>
2026-05-26 22:59:21 -0400

    sleep instead

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e 
 if [ "$DEBUG" = 1 ]; then
-    exec tmux new-session
+    exec sleep infinity
 fi
 cd /server
 exec wine system/ucc.exe server KF-bioticslab.rom?game=KFmod.KFGameType?VACSecured=false?MaxPlayers=6?AdminName=admin?AdminPassword=$ADMIN_PASSWORD
